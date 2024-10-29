121359f86fafd913495b3010534e7103788005af
owen <youngwhenbold@outlook.com>
2024-10-29 11:48:41 +0800

    depoly test

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -6,7 +6,8 @@ function Blog() {
   const [blogs, setBlogs] = useState([]);
 
   const fetchMovies = () => {
-    fetch("http://localhost:3000/data/blogData.json")
+    const dataUrl = `${process.env.REACT_APP_API_URL}/assets/data/blogData.json`;
+    fetch(dataUrl)
       .then((res) => res.json())
       .then((data) => {
         setBlogs(data);
